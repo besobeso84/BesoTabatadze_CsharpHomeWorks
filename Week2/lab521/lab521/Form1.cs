a2d628df9d76bc4aad2f4071beb72a7c74e4ca0c
besobeso84 <btabatadze84@gmail.com>
2019-04-10 12:37:37 +0400

    Week2
    lab521

---
+++ Week2/lab521/lab521/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab521
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] nishnebi = {100, 89, 67, 23, 45, 56, 89, 65, 98, 81};
+            studenti studentObject = new studenti();
+            label1.Text = studentObject.sashualoAritmetikuli(nishnebi).ToString();
+        }
+    }
+}
---
+++ Week2/lab521/lab521/studenti.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab521
+{
+    class studenti
+    {
+        public double sashualoAritmetikuli(int[] nishnebi)
+        {
+            double sashualo=0, jami=0;
+            for(int i=0; i<nishnebi.Length; i++)
+            {
+                jami += nishnebi[i]; 
+            }
+            sashualo = jami / nishnebi.Length;
+            return sashualo;
+        }
+    }
+}
